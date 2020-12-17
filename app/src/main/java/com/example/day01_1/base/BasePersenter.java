d75b4a9cd7b777193ed1c3d5cef845f23838bb61
zm05130513 <zm05130513@163.com>
2020-12-17 23:33:09 +0800

    BasePersenter.java

--- app/src/main/java/com/example/day01_1/base/BasePersenter.java
+++ app/src/main/java/com/example/day01_1/base/BasePersenter.java
@@ -2,7 +2,8 @@ package com.example.day01_1.base;
 
 public class BasePersenter<V extends BaseView> {
     public V iview;
-    public void attachView(V v){
-        iview=v;
+
+    public void attachView(V v) {
+        iview = v;
     }
 }
